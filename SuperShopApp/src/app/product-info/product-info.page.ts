04456956a247c8d1863e9b56c77e6c6048c3be98
THILAD <laoappsdev3@gmail.com>
2020-12-04 17:05:26 +0700

    commit

--- SuperShopApp/src/app/product-info/product-info.page.ts
+++ SuperShopApp/src/app/product-info/product-info.page.ts
@@ -7,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductInfoPage implements OnInit {
 
-  constructor() { }
 
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    startAutoplay:true
+  };
+  constructor() { }
+  slidesDidLoad(slides) {
+    slides.startAutoplay();
+  }
   ngOnInit() {
   }
 
